737e53b62cda7ddececd680d32229a17188258e7
Marco Rocheles <marcorocheles@hotmail.com>
2021-12-03 22:13:07 -0300

    Relação Entre tabelas

--- BlogPessoal 01-12-2021/blogPessoal/src/main/java/org/generation/blog/model/Postagem.java
+++ BlogPessoal 01-12-2021/blogPessoal/src/main/java/org/generation/blog/model/Postagem.java
@@ -6,11 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //Model é a classe responsável pela comunicação com o banco de dados, onde fica a tabela
 @Entity //Anotação que indica que essa classe fará relação com o mySQL
@@ -33,6 +38,12 @@ public class Postagem {
 		@Temporal(TemporalType.TIMESTAMP)
 		private Date data = new java.sql.Date(System.currentTimeMillis()); 
 		//assim que passar um dado por esta classe ele irá capturar a data, hora e segundo
+		
+		@ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Tema tema;
+		
+		
 		
 		//gerando getters and setters
 		public long getId() {
@@ -59,6 +70,12 @@ public class Postagem {
 		public void setData(Date data) {
 			this.data = data;
 		}
-		
+
+		public Tema getTema() {
+			return tema;
+		}
+		public void setTema(Tema tema) {
+			this.tema = tema;
+		}
 		
 }
